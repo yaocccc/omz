849fb714f84da1dea083189b36847f0d607f3114
chenyc <flyforest@outlook.com>
2022-11-04 16:39:30 +0800

    fix hook.zsh

--- config/hook.zsh
+++ config/hook.zsh
@@ -3,7 +3,7 @@ preexec_hook() { _cmd=($(echo $2)); print -n "\e]2;${(q)_cmd[1]}\a"; }
 add-zsh-hook -Uz preexec preexec_hook
 
 # auto to last pwd
-chpwd_hook() { echo $PWD > $ZSH/cache/currentdir }
+chpwd_hook() { echo $PWD > $OMZ/cache/currentdir }
 add-zsh-hook -Uz chpwd chpwd_hook
-currentdir=$(cat $ZSH/cache/currentdir 2>/dev/null)
+currentdir=$(cat $OMZ/cache/currentdir 2>/dev/null)
 [ -d "$currentdir" ] && cd $currentdir
