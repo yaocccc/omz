07d600c9ccf8a6491fb113a7d0d5f7ec160e62da
chenyc <flyforest@outlook.com>
2022-11-07 11:36:22 +0800

    添加配置控制是否使用hook

--- config/hook.zsh
+++ config/hook.zsh
@@ -1,9 +1,16 @@
 # auto update the terminal name
-preexec_hook() { _cmd=($(echo $2)); print -n "\e]2;${(q)_cmd[1]}\a"; }
-add-zsh-hook -Uz preexec preexec_hook
+_apply_preexec_hook() {
+    preexec_hook() { _cmd=($(echo $2)); print -n "\e]2;${(q)_cmd[1]}\a"; }
+    add-zsh-hook -Uz preexec preexec_hook
+}
 
 # auto to last pwd
-chpwd_hook() { echo $PWD > $OMZ/cache/currentdir }
-add-zsh-hook -Uz chpwd chpwd_hook
-currentdir=$(cat $OMZ/cache/currentdir 2>/dev/null)
-[ -d "$currentdir" ] && cd $currentdir
+_apply_chpwd_hook() {
+    chpwd_hook() { echo $PWD > $OMZ/cache/currentdir }
+    add-zsh-hook -Uz chpwd chpwd_hook
+    currentdir=$(cat $OMZ/cache/currentdir 2>/dev/null)
+    [ -d "$currentdir" ] && cd $currentdir
+}
+
+[ "$_OMZ_APPLY_PREEXEC_HOOK" != "false" ] && _apply_preexec_hook
+[ "$_OMZ_APPLY_CHPWD_HOOK" != "false" ] && _apply_chpwd_hook
