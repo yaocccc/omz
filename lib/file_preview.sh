d136130771dda72653d32d52b8ba1ef284c5bfb8
chenyc <flyforest@outlook.com>
2022-11-17 14:11:11 +0800

    fix: file_preview mac系统下ls错误

--- lib/file_preview.sh
+++ lib/file_preview.sh
@@ -2,7 +2,7 @@
 mime=$(file -bL --mime-type "$1")
 category=${mime%%/*}
 if [ -d "$1" ]; then
-    exa -l --no-user --no-time --icons "$1" 2>/dev/null || ls --color=tty "$1" || ls -G "$1"
+    exa -l --no-user --no-time --icons "$1" 2>/dev/null || ls --color=tty "$1" 2>/dev/null || ls -G "$1"
 elif [ "$category" = text ]; then
     (bat -p --color=always "$1" || cat "$1") 2>/dev/null | head -100
 elif [ "$category" = image ]; then
