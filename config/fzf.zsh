713d8485bb53d287a8efc502611c89bdf43943c3
chenyc <flyforest@outlook.com>
2022-11-18 14:10:23 +0800

    update

--- config/fzf.zsh
+++ config/fzf.zsh
@@ -1,4 +1,4 @@
-alias fzf='fzf --preview "bash $OMZ/lib/file_preview.sh {}" --height 12'
+alias fzf='bash $OMZ/lib/get_cursor.sh; fzf --preview "bash $OMZ/lib/file_preview.sh {}" --height 12'
 
 export FZF_DEFAULT_OPTS="--height 12 --layout=reverse --history=$OMZ/cache/fzfhistory"
 export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,dist,vendor} --type f"
