7f060f5527ec06b71d30de1de1b65730f3c58874
chenyc <flyforest@outlook.com>
2023-04-24 12:15:55 +0800

    update exa preview style

--- lib/file_preview.sh
+++ lib/file_preview.sh
@@ -2,7 +2,7 @@
 mime=$(file -bL --mime-type "$1")
 category=${mime%%/*}
 if [ -d "$1" ]; then
-    exa -l --no-user --no-time --icons "$1" 2>/dev/null || ls --color=always "$1" 2>/dev/null || ls -G "$1"
+    exa -l --no-user --no-time --icons --no-permissions --no-filesize "$1" 2>/dev/null || ls --color=always "$1" 2>/dev/null || ls -G "$1"
 elif [ "$category" = text ]; then
     (bat -p --color=always "$1" || cat "$1") 2>/dev/null | head -1000
 elif [ "$category" = image ]; then
