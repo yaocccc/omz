3a2df05e6bff546da0d252290bbba333475ad4a0
chenyc <flyforest@outlook.com>
2026-06-26 12:33:22 +0800

    update: 使用FZFPREVIEW自带的参数替换掉原来的get_cursor.sh脚本

--- lib/img_preview.sh
+++ lib/img_preview.sh
@@ -3,24 +3,28 @@
 declare -r -x DEFAULT_PREVIEW_POSITION="right"
 declare -r -x PREVIEW_ID="preview"
 
-source $OMZ/cache/cursor
-X=$(($COLUMNS / 2 + 2))
-Y=$((row + 2))
-if [ $Y -gt $((LINES - 11)) ]; then
-    Y=$((LINES - 11))
-fi
+TERM_COLUMNS=$(tput cols 2>/dev/null || echo 80)
+TERM_LINES=$(tput lines 2>/dev/null || echo 24)
+
+PREVIEW_LEFT=${FZF_PREVIEW_LEFT:-$((TERM_COLUMNS / 2))}
+PREVIEW_TOP=${FZF_PREVIEW_TOP:-$((TERM_LINES > 12 ? TERM_LINES - 12 : 0))}
+PREVIEW_COLUMNS=${FZF_PREVIEW_COLUMNS:-$((TERM_COLUMNS - PREVIEW_LEFT))}
+PREVIEW_LINES=${FZF_PREVIEW_LINES:-12}
 
 function draw_preview {
-    ueberzugpp cmd -s $UEBERZUGPP_SOCKET -i $PREVIEW_ID -a add \
-        -x $X -y $Y --max-width $(($COLUMNS / 2 - 2)) --max-height 10 \
-        -f ${@}
+    local file=$1
+    [ -n "$file" ] || return 1
+
+    ueberzugpp cmd -s "$UEBERZUGPP_SOCKET" -i "$PREVIEW_ID" -a add \
+        -x "$PREVIEW_LEFT" -y "$PREVIEW_TOP" --max-width "$PREVIEW_COLUMNS" --max-height "$PREVIEW_LINES" \
+        -f "$file"
 }
 
 function remove_preview {
-    ueberzugpp cmd -s $UEBERZUGPP_SOCKET -i $PREVIEW_ID -a remove
+    ueberzugpp cmd -s "$UEBERZUGPP_SOCKET" -i "$PREVIEW_ID" -a remove
 }
 
 case "$1" in
-	draw) shift; draw_preview $* ;;
+	draw) shift; draw_preview "$@" ;;
 	remove) remove_preview ;;
 esac
